4e364519a9bbd85bbcf2c45dc24d4a7947a928ac
Andrew Steinmeyer <jandrewsteinmeyer@gmail.com>
2015-10-03 09:13:33 -0400

    View extracts data from broadcast
    Configuring the collection view cell to extract data from firebase.  The view cell will be loaded with the data for that specific broadcast.  SDWebImage will request the cached product images from Fastly.

--- ePortal/DiscoverViewCell.swift
+++ ePortal/DiscoverViewCell.swift
@@ -7,26 +7,74 @@
 //
 
 import UIKit
-
-// TODO: Shell, need to add stuff
+import WebImage
 
 class DiscoverViewCell: UICollectionViewCell {
   
-  override init(frame: CGRect) {
-    super.init(frame: frame)
-    
+  @IBOutlet weak var publisherThumbnailView: UIImageView!
+  @IBOutlet weak var publisherNameLabel: UILabel!
+  @IBOutlet weak var priceLabel: UILabel!
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var productMaskView: UIView!
+  @IBOutlet weak var productImageView: UIImageView!
+  @IBOutlet weak var itemsRemainingButton: DesignableButton!
+  
+  override func awakeFromNib() {
+    // setup cell
+    //loadPlaceholderData()
     setupAppearance()
     
   }
-
-  required init?(coder aDecoder: NSCoder) {
-    super.init(coder: aDecoder)
-    
-    setupAppearance()
+  
+  func loadPlaceholderData() {
+    publisherThumbnailView.image = UIImage(named: "carrie-profile")
+    publisherNameLabel.text = "Carrie Steinmeyer"
+    titleLabel.text = "Biker Jacket"
+    priceLabel.text = "$85"
+    productImageView.image = UIImage(named: "biker-jacket")
+    itemsRemainingButton.titleLabel?.text = "3"
   }
   
   func setupAppearance() {
     //contentView.layer.cornerRadius = 5
+  }
+  
+  func configureCellWithSnapshot(snapshot: FDataSnapshot) {
+    let val: AnyObject! = snapshot.value
+    
+    if (val != nil) {
+      // update data for user from firebase snapshot
+      let data = JSON(val)
+      let title = data["title"].string ?? ""
+      let price = data["price"].double ?? 0
+      let quantity = data["quantity"].int ?? 0
+      let photokey = data["photos"]["image1"].string ?? ""
+      
+      
+      self.titleLabel.text = title
+      self.priceLabel.text = "$\(String(price))"
+      self.itemsRemainingButton.setTitle(String(quantity), forState: .Normal)
+      
+      let cacheUrl = Constants.Fastly.RootUrl.stringByAppendingString(photokey)
+      
+      print(cacheUrl)
+      let url = NSURL(string: cacheUrl)
+      self.productImageView.sd_setImageWithURL(url!, completed: {
+        (image: UIImage!, error: NSError!, type: SDImageCacheType, url: NSURL!) in
+        
+        if error != nil {
+          print(error)
+        }
+        else {
+          print("hooray")
+        }
+        
+      })
+      
+      // TODO: Slow downloading photos
+      // start download of image from AWS S3
+      //S3Handler.sharedInstance.downloadImageForCell(self, withBucketKey: photokey)
+    }
   }
   
 }
